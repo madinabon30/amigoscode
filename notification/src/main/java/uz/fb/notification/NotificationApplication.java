8e98680f2698c4a29cca7ada4de02c05dadde506
madinabonu.shirinkulova <madinabonu.shirinkulova@fido.uz>
2024-01-17 18:35:00 +0500

    messaging queue

--- notification/src/main/java/uz/fb/notification/NotificationApplication.java
+++ notification/src/main/java/uz/fb/notification/NotificationApplication.java
@@ -1,9 +1,12 @@
 package uz.fb.notification;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import uz.fb.amqp.RabbitMQMessageProducer;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -18,4 +21,15 @@ public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(
+//            RabbitMQMessageProducer producer,
+//            NotificationConfig notificationConfig
+//    ){
+//        return args -> {
+//            producer.publish("foo", notificationConfig.getInternalExchange(), notificationConfig.getInternalNotificationRoutingKey());
+//        };
+//    }
+
 }
